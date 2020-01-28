101975c683dffded28089ab42fec6043ccb9a3a5
JorgePastorCastilla <jordi131313@gmail.com>
2020-01-28 12:07:17 +0100

    afegir y consulta medio hechos

--- app/src/main/java/com/example/activitat2/Afegir.java
+++ app/src/main/java/com/example/activitat2/Afegir.java
@@ -3,12 +3,43 @@ package com.example.activitat2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Afegir extends AppCompatActivity {
+public class Afegir extends AppCompatActivity implements View.OnClickListener{
+
+    DBInterface bd;
+    EditText editNom;
+    EditText editEmail;
+    Button btnAfegir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_afegir);
+
+        editNom = findViewById(R.id.editTextNomAfegir);
+        editEmail = findViewById(R.id.editTextEmailAfegir);
+        btnAfegir = (Button) findViewById(R.id.buttonAfegirAfegir);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnAfegir) {
+            bd = new DBInterface(this);
+            bd.obre();
+            if (bd.insereixContacte(editNom.getText().toString(),
+                    editEmail.getText().toString()) != -1) {
+                Toast.makeText(this, "Afegit correctament",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Error a l’afegir",
+                        Toast.LENGTH_SHORT).show();
+            }
+            bd.tanca();
+            finish();
+        }
     }
 }
